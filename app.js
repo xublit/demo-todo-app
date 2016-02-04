5efd820da01fbb081e52a9a9d2b3fbc45cd00a8d
Cal Milne <cal@calmilne.com>
2016-02-05 08:10:37 +1100

    XublitApp.provide()

--- app.js
+++ app.js
@@ -3,19 +3,69 @@
 // Prevent inevitable timezone confusion between servers, UIs, and APIs
 process.env.TZ = 'UTC';
 
-// Tell us about any uncaught errors
-process.on('uncaughtException', function (err) {
-    console.log(err);
-});
+// Get the environment name from the ENVIRONMENT_NAME environment variable if
+// it has been set, otherwise use 'local'
+var environmentName = process.env.ENVIRONMENT_NAME || 'local';
 
 // Load in our app class
 var ThingsTodoApp = require('./src/thingsTodo');
 
 // Create an instance of our app
 var thingsTodoApp = new ThingsTodoApp({
+
+    env: environmentName,
+
     baseDir: __dirname,
+    srcDir: './src',
+    etcDir: './etc',
+
+    /**
+     * Additional dependencies to make available
+     */
+    provide: {
+
+        /**
+         * Creates an injectable instance of the XublitMongoDb module, which
+         * will be bootstrapped with the configuration options specified
+         * the configuration specified, dependency with the reference '$mongoDb'
+         */
+        $mongoDb: {
+            usingConfiguration: 'mongodb',
+            createInstanceOf: 'XublitMongoDb',
+        },
+
+        $restServer: {
+            usingConfiguration: 'restServer',
+            createInstanceOf: 'XublitRestServer',
+        },
+
+    },
+
+});
+
+// MongoDB
+thingsTodoApp.provide('$mongoDb', {
+    usingConfiguration: 'mongodb',
+    createInstanceOf: 'XublitMongoDb',
+});
+
+// REST Server
+thingsTodoApp.provide('$restServer', {
+    usingConfiguration: 'restServer',
+    createInstanceOf: 'XublitRestServer',
+});
+
+// Relational Models
+thingsTodoApp.provide('$models', {
+    usingConfiguration: 'models',
+    createInstanceOf: 'XublitRelationalModels',
 });
 
 // Start it up!
 // Bootstrap modules, emit startup events, etc.
 thingsTodoApp.start();
+
+// Log any uncaught errors in console
+process.on('uncaughtException', function (err) {
+    console.log(err);
+});
